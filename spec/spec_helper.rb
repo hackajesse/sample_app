9923b54d7e52068babf2401a9d57b9b63d91b8eb
Jesse Biroscak <hackajesse@gmail.com>
2013-07-18 10:24:24 -0400

    Add a full_title helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,7 @@ Spork.prefork do
     # Run specs in random order to surface order dependencies. If you find an
     # order dependency and want to debug it, you can fix the order by providing
     # the seed, which is printed after each run.
-    #     --seed 1234
+    # --seed 1234
     config.order = "random"
     # Include the Capybara DSL so that specs in spec/requests still work.
     config.include Capybara::DSL
@@ -100,7 +100,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   # ## Mock Framework
